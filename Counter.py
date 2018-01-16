de4ddd3ecfaf31c7477b0ccb60d8ee2dfc95f4d4
thr3 <fa.martin.carlsson@gmail.com>
2018-01-16 19:44:50 +0100

    initial commit

--- Counter.py
+++ Counter.py
@@ -1,6 +1,10 @@
 from collections import Counter
-c = Counter('hello world')
-print (c)
+#print ("Write a word")
+c=raw_input("Write a word\n")
+#c = Counter('hello world')
+a = Counter(c)
+print (a)
 #Counter({'l': 3, 'o': 2, ' ': 1, 'e': 1, 'd': 1, 'h': 1, 'r': 1, 'w': 1})
-print (c.most_common(2))
+print ("Most common are ")
+print (a.most_common(3))
 #[('l', 3), ('o', 2)]
